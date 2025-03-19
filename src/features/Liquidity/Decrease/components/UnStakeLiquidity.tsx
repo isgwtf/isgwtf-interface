a98375c326bd0f14928c9d1d997f4aa6474e0a0d
s01ana <sentasmer@gmail.com>
2025-03-20 03:07:09 +1000

    remove api in portfolio page

--- src/features/Liquidity/Decrease/components/UnStakeLiquidity.tsx
+++ src/features/Liquidity/Decrease/components/UnStakeLiquidity.tsx
@@ -68,8 +68,8 @@ export default function UnStakeLiquidity({
   })
 
   const v1Balance = farmPositionData?.data
-    .filter((d) => d.version === 'V1' && d.userVault !== vault && d.lpAmount !== '0')
-    .reduce((acc, cur) => acc.add(cur.lpAmount), new Decimal(0))
+    .filter((d: any) => d.version === 'V1' && d.userVault !== vault && d.lpAmount !== '0')
+    .reduce((acc: any, cur: any) => acc.add(cur.lpAmount), new Decimal(0))
     ?.toString()
   const v1Deposited = new Decimal(v1Balance || 0).div(10 ** (selectedFarm?.lpMint.decimals || 0)).toString()
 
